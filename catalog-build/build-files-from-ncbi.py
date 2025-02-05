a01ec0ff4a5a914e966af5e330f1652e95becfb6
hunterckx <118154470+hunterckx@users.noreply.github.com>
2025-02-04 20:48:52 -0800

    feat: add tol id to catalog source (#10)

--- catalog-build/build-files-from-ncbi.py
+++ catalog-build/build-files-from-ncbi.py
@@ -16,7 +16,18 @@ TAXONOMIC_GROUPS_BY_TAXONOMY_ID = {
   1476529: "Fish",
 }
 
+TOLIDS_BY_TAXONOMY_ID = {
+  40674: "m",
+  8782: "b",
+  8504: "r",
+  8459: "r",
+  1294634: "r",
+  8292: "a",
+  7898: "f",
+}
+
 if __name__ == "__main__":
   build_files(ASSEMBLIES_PATH, GENOMES_OUTPUT_PATH, UCSC_ASSEMBLIES_URL, {
     "taxonomicGroup": TAXONOMIC_GROUPS_BY_TAXONOMY_ID,
+    "tolId": TOLIDS_BY_TAXONOMY_ID,
   })
