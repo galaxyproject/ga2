12c2ab7d39bffef285264d5bb76ee0913ba55258
hunterckx <118154470+hunterckx@users.noreply.github.com>
2025-02-05 19:37:52 -0800

    feat: add mutually exclusive taxonomic groups and tol ids for current organisms (#10)

--- catalog-build/build-files-from-ncbi.py
+++ catalog-build/build-files-from-ncbi.py
@@ -9,21 +9,41 @@ GENOMES_OUTPUT_PATH = "catalog-build/source/genomes-from-ncbi.tsv"
 TAXONOMIC_GROUPS_BY_TAXONOMY_ID = {
   40674: "Mammalia",
   8782: "Aves",
-  8457: "Sauropsida",
+  8459: "Testudines",
+  8504: "Lepidosauria",
+  1294634: "Crocodylia",
   8292: "Amphibia",
-  7898: "Fish",
-  7777: "Fish",
-  1476529: "Fish",
+  7898: "Actinopterygii",
+  7777: "Chondrichthyes",
+  1476529: "Cyclostomata",
+  7894: "Coelacanthiformes",
+  7878: "Dipnomorpha",
+  2682552: "Leptocardii",
+  7712: "Tunicata",
+  10219: "Hemichordata",
+  7586: "Echinodermata",
+  6447: "Mollusca",
+  50557: "Insecta",
 }
 
 TOLIDS_BY_TAXONOMY_ID = {
-  40674: "m",
-  8782: "b",
-  8504: "r",
-  8459: "r",
-  1294634: "r",
-  8292: "a",
-  7898: "f",
+  40674: "m", # Mammalia
+  8782: "b", # Aves
+  32561: { # Sauria
+    "value": "r",
+    "exclude": 8782 # Aves
+  },
+  8292: "a", # Amphibia
+  7898: "f", # Actinopterygii
+  7777: "s", # Chondrichthyes
+  7711: { # Chordata
+    "value": "k",
+    "exclude": [40674, 32561, 8292, 7898, 7777] # Mammalia, Sauria, Amphibia, Actinopterygii, Chondricthyes
+  },
+  10219: "k", # Hemichordata
+  7586: "e", # Echinodermata
+  6447: "x", # Mollusca
+  50557: "i", # Insecta
 }
 
 if __name__ == "__main__":
